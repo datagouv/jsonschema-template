66335906fabc30ddd06d07850168be0503a7203b
Pierlou <pierlou.ramade@data.gouv.fr>
2024-08-27 16:38:47 +0200

    feat: allow not to have example

--- .github/workflows/integrity_check.py
+++ .github/workflows/integrity_check.py
@@ -1,10 +1,16 @@
 import json
+import os
+import logging
 from jsonschema import validators, validate
 
 with open("schema.json", "r") as f:
     schema_data = json.load(f)
 validators.validator_for(schema_data).check_schema(schema_data)
 
-with open("exemple-valide.json", "r") as f:
-    data = json.load(f)
-validate(instance=data, schema=schema_data)
+if "exemple-valide.json" in os.listdir():
+    with open("exemple-valide.json", "r") as f:
+        data = json.load(f)
+    validate(instance=data, schema=schema_data)
+
+else:
+    logging.warning('No example file to validate, consider adding one')
