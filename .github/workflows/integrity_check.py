609199dadcea9cfbcdee79c5eaac33f386426199
Pierlou <pierlou.ramade@data.gouv.fr>
2024-08-27 16:19:35 +0200

    feat: initial commit

---
+++ .github/workflows/integrity_check.py
@@ -0,0 +1,10 @@
+import json
+from jsonschema import validators, validate
+
+with open("schema..json", "r") as f:
+    schema_data = json.load(f)
+validators.validator_for(schema_data).check_schema(schema_data)
+
+with open("exemple-valide.json", "r") as f:
+    data = json.load(f)
+validate(instance=data, schema=schema_data)
